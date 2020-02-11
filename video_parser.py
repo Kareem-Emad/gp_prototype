a28748f1f38aad2cef0d3e108d9e3191ef4448c4
kareem <kareememad400@gmail.com>
2020-02-11 16:56:36 +0200

    face recognizer model adapted in execution flow

--- video_parser.py
+++ video_parser.py
@@ -1,6 +1,7 @@
 import cv2  # noqa
 from model_manager import load_model_config
 from video_processor import video_processor
+import time
 
 
 def parse_source(filename, frames_per_exec, model_obj, vid_processor):
@@ -15,8 +16,11 @@ def parse_source(filename, frames_per_exec, model_obj, vid_processor):
 
     frame_buffer = []
     processed_frames_count = 0
+    average_fps_computation_time = 0
 
-    while (video_buffer.isOpened()):
+    start_time = time.time()
+
+    while (video_buffer.isOpened() and processed_frames_count <= total_frames_count):
         ret, frame = video_buffer.read()
 
         print(f'Processing Video {processed_frames_count}/{total_frames_count} frames processed\r', end="")
@@ -31,6 +35,13 @@ def parse_source(filename, frames_per_exec, model_obj, vid_processor):
 
         processed_frames_count += 1
 
+        if(processed_frames_count % fps == 0):
+            average_fps_computation_time += time.time() - start_time
+            start_time = time.time()
+
+    print(f"estimated video second model execution time = {average_fps_computation_time/(total_frames_count/fps)} seconds") # noqa
+    print(f"estimated total processing time = {average_fps_computation_time} seconds")
+
     video_buffer.release()
 
 
@@ -42,7 +53,7 @@ def get_fps(video_buffer):
 
 def get_frames_count(video_buffer):
     frame_count = int(video_buffer.get(cv2.CAP_PROP_FRAME_COUNT))
-    print(f"Frames per second: {frame_count}")
+    print(f"Total frames count: {frame_count}")
     return frame_count
 
 
