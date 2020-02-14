a9aef2fc36f5a203f5fa21ede7d73a545a128327
kareem <kareememad400@gmail.com>
2020-02-14 16:01:46 +0200

    write continous periods to disk on every flush

--- video_processor.py
+++ video_processor.py
@@ -1,3 +1,5 @@
+import json
+import time
 from model_manager import execute_model
 from video_indexer import video_indexer
 
@@ -11,6 +13,17 @@ class video_processor:
         self.frames_per_exec = frames_per_exec
         self.num_flushed = 0
         self.vid_indexer = video_indexer()
+        self.indexed_data = self.read_stored_indexed_clips('./index_database.json')
+
+    def read_stored_indexed_clips(self, file_path):
+        try:
+            with open(file_path, 'r') as f:
+                json_str = f.read()
+                json_data = json.loads(json_str)
+                return json_data
+        except Exception:
+            return {'data': []}
+        return {'data': []}
 
     def process_frames(self, frame_buffer, model_obj):
         tag = execute_model(frame_buffer, model_obj)
@@ -31,6 +44,18 @@ class video_processor:
 
         for clip in clips:
             start_period_frame, end_period_frame = clip
+
             start_period_frame, end_period_frame = start_period_frame + starting_frame_index, end_period_frame + starting_frame_index
             start_period_second, end_period_second = int(start_period_frame / self.fps), int(end_period_frame / self.fps)
-            print(f"Interest Period start between  {start_period_second} and {end_period_second} in video")
+
+            start_period_second = time.strftime('%H:%M:%S', time.gmtime(start_period_second))
+            end_period_second = time.strftime('%H:%M:%S', time.gmtime(end_period_second))
+
+            print(f"Interest Period between  {start_period_second} and {end_period_second} in video")
+            self.indexed_data['data'].append({'start_at': start_period_second, 'end_at': end_period_second})
+
+        try:
+            with open('./index_database.json', 'w') as f:
+                f.write(json.dumps(self.indexed_data))
+        except Exception:
+            print("Failed To write updated index to database, will retry next flush isa")
